bd65a558bf771a02440ea9a2e7cfa30c18c9d677
ParisGharbi <63523680+ParisGharbi@users.noreply.github.com>
2020-04-13 18:15:03 -0400

    Create Volume of Cylinder.py

---
+++ Volume of Cylinder.py
@@ -0,0 +1,9 @@
+# Python Program to find Volume & Surface Area of a Cylinder
+
+PI = 3.14
+radius = float(input('Please Enter the Radius of a Cylinder: '))
+height = float(input('Please Enter the Height of a Cylinder: '))
+
+Volume = PI * radius * radius * height
+
+print(" The Volume of a Cylinder = %.2f" %Volume)
