458e7243e965e8c783285f84649b761e459480ee
ParisGharbi <63523680+ParisGharbi@users.noreply.github.com>
2020-04-17 00:59:35 -0400

    Create units of time(again).py

---
+++ units of time(again).py
@@ -0,0 +1,24 @@
+#Convert a number of seconds to days, hours, minutes and seconds.
+
+SECONDS_PER_DAY = 86400
+SECONDS_PER_HOUR = 3600
+SECONDS_PER_MINUTE = 60
+
+#Read input from user
+seconds = int(input("Enter a number of seconds: "))
+
+#Compute days, hours, minutes and seconds
+days = seconds / SECONDS_PER_DAY
+seconds = seconds % SECONDS_PER_DAY
+
+hours = seconds / SECONDS_PER_HOUR
+seconds = seconds % SECONDS_PER_HOUR
+
+minutes = seconds / SECONDS_PER_MINUTE
+seconds = seconds % SECONDS_PER_MINUTE
+
+#Display result 
+print("The equivalent duration is", \
+"%d:%02d:%02d:%02d. " % (days, hours, minutes, seconds))
+
+
